e9b7cfd485b3f7c5bfdc042494055a9590588498
tejiritohabru <tejiritohabru04@gmail.com>
2025-03-08 10:55:55 +0100

    i dded my assignment to the program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -5,5 +5,14 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+    
+
+
+Console.Write("What is your first name?");
+string answer = Console.ReadLine();
+Console.Write("What is your last name?");
+string name = Console.ReadLine();
+Console.WriteLine($"your name is {name}, {answer} {name}.");
+
     }
 }
--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Formats.Asn1;
 
 class Program
 {
